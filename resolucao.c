83d52fba516a03d75bb1ed6d5d6df6375670634a
Rodrigo Suélli <rodrigosuelli@gmail.com>
2022-02-17 12:38:41 -0300

    First commit

---
+++ resolucao.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+const int STACK_LENGTH = 4;
+int position = 0;
+
+void push(int number, int arr[]);
+void pop(int arr[]);
+print_array(int arr[]);
+fill_array_with_zeros(int arr[]);
+
+int main(void)
+{
+    int stack[STACK_LENGTH];
+    fill_array_with_zeros(stack);
+
+    push(1, stack);
+    push(1, stack);
+    push(1, stack);
+    push(1, stack);
+
+    pop(stack);
+    pop(stack);
+    pop(stack);
+    pop(stack);
+
+    printf("posicao: %d\n", position);
+    puts("pilha:");
+    print_array(stack);
+    return 0;
+}
+
+void push(int number, int arr[])
+{
+    // aceita da posicao 0 a 3 do array, ou seja, da primeira ate a ultima
+    if (position >= 0 && position <= STACK_LENGTH - 1)
+    {
+        arr[position] = number;
+        position++;
+    }
+    else
+    {
+        puts("Erro, a pilha esta cheia.");
+    }
+}
+
+void pop(int arr[])
+{
+    // aceita da posicao 1 a 4 do array
+    if (position >= 1 && position <= STACK_LENGTH)
+    {
+        arr[position-1] = 0;
+        position--;
+    }
+    else
+    {
+        puts("Erro, a pilha esta vazia.");
+    }
+}
+
+print_array(int arr[])
+{
+    int i;
+    for (i = 0; i < STACK_LENGTH; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+}
+
+fill_array_with_zeros(int arr[])
+{
+    int i;
+    for (i = 0; i < STACK_LENGTH; i++)
+    {
+        arr[i] = 0;
+    }
+}
